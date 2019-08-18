815c52d16e80b3b32c61dae7d3ca8773a1ef7802
Thiago dos Santos Silva <thiagosantos346@gmail.com>
2019-08-18 19:54:03 +0000

    Create de tests cases and fix bugs

--- booktime/booktime/main/forms.py
+++ booktime/booktime/main/forms.py
@@ -1,5 +1,22 @@
 from django import forms
+from django.core.mail import send_mail
+import logging
 
-class ContactForm(forms.form):
-    name = form.CharField(label='Your Name', max_length=100)
-    message = form.CharField(max_length=600, widget=forms.Textarea)
+logger = logging.getLogger(__name__)
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Your Name', max_length=100)
+    message = forms.CharField(max_length=600, widget=forms.Textarea)
+
+    def send_mail(self):
+        logger.info("Sending email to customer service")
+        message = "Form: {0}\n{1}".format(
+            self.cleaned_data["name"],
+            self.cleaned_data["message"])
+        send_mail(
+            "Site message",
+            message,
+            "site@booktime.domain",
+            ["customerservice@booktime.domain"],
+            fail_silently = False
+        )
